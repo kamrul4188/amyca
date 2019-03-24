1fef846a2980a59001cd7f16e24cf974908d30d8
kamrul4188 <kamrul4188@gmail.com>
2019-03-24 22:59:26 +0800

    1. Fixed bug

--- amyca/amyca_v2.py
+++ amyca/amyca_v2.py
@@ -45,6 +45,11 @@ class User:
 			raise IndexError('Index is our of range')
 
 	@classmethod
+	def remove(cls, name):
+		# Todo: add functionality to remove user
+		pass
+
+	@classmethod
 	def get_total(cls):
 		return cls.__total
 
@@ -82,9 +87,18 @@ class Print:
 
 	@classmethod
 	def greeting(cls):
-
 		banner = pyfiglet.figlet_format('Welcome to AMYCA')
 		print(Fore.MAGENTA + banner)
+
+	@classmethod
+	def help(cls):
+		pass
+		# TODO: add all command instruction with example
+
+	@classmethod
+	def users(cls):
+		pass
+		# TODO: add functionality to print user info
 
 
 class Process:
@@ -115,7 +129,7 @@ class Process:
 		print('>>> Are you sure? y/n')
 		response = input()
 		if response.lower() == 'y':
-			print(">>> Bye!")
+			print(pyfiglet.figlet_format('>>> Bye!', font='digital'))
 			sys.exit()
 		elif response.lower() == 'n':
 			print(pyfiglet.figlet_format('Welcome back !', font='digital'))
@@ -126,23 +140,46 @@ class Process:
 	@classmethod
 	def add_user(cls, user_input):
 		"""
-		:param user_input: command + user parameter
-		:parameter 0 and 1: command
-		:parameter 2: user name
-		:parameter 3: user password
-		:parameter 4: user access level
+		:param user_input:
+		parameter 0 and 1: command
+			:parameter: 2 user name
+			:parameter 3: user password
+			:parameter 4: user access level
 		:return: null
 		"""
 		if User.get_current_user_access_level() == 4:
-			name = user_input.split(' ', 5)[2]
-			password = user_input.split(' ', 5)[3]
-			level = user_input.split(' ', 5)[4]
-			User(name, password, level)
-			msg_1 = Back.LIGHTYELLOW_EX + ' A user has  been added as ' + Back.LIGHTYELLOW_EX + Fore.RED + name
-			msg_2 = Back.LIGHTYELLOW_EX + ' with level of access is : ' + Back.LIGHTYELLOW_EX + Fore.RED + str(level) + ' '
-			print(msg_1 + msg_2)
+			try:
+				name = user_input.split(' ', 5)[2]
+				password = user_input.split(' ', 5)[3]
+				level = user_input.split(' ', 5)[4]
+				User(name, password, level)
+				msg_1 = Back.LIGHTYELLOW_EX + ' A user has  been added as ' + Back.LIGHTYELLOW_EX + Fore.RED + name
+				msg_2 = Back.LIGHTYELLOW_EX + ' with level of access is : ' + Back.LIGHTYELLOW_EX + Fore.RED + str(level) + ' '
+				print(msg_1 + msg_2)
+			except IndexError:
+				raise IndexError('Invalid input...!!!. Please use AMYCA CLI.')
 		else:
 			raise ValueError('Only admin can add new user')
+
+	@classmethod
+	def remove_user(cls):
+		# TODO: Update functionality (only admin can remove)
+		pass
+
+	@classmethod
+	def change_user_password(cls):
+		# TODO: add functionality (only admin can change)
+		pass
+
+	@classmethod
+	def change_user_access_level(cls):
+		# TODO: add functionality (only admin can change)
+		pass
+
+	@classmethod
+	def add_project(cls):
+		# Todo: add functionality (only manage can add project)
+		pass
 
 
 class Command:
@@ -157,7 +194,7 @@ class Command:
 	@classmethod
 	def execute(cls, command):
 		if command == '':
-			raise ValueError('You did not input anything. you can type \'help\' for more info')
+			raise ValueError('You did not input anything.')
 		elif command == 'exit':
 			Process.terminate()
 		elif command == 'logout':
@@ -167,22 +204,29 @@ class Command:
 		else:
 			raise ValueError('command not recognized')
 
+		# TODO: add all command to be execute on amyca_v1.0
+
 
 def main():
 	User('admin', 'admin123', 4)
-	Process.login()
-	# Print.greeting()
+	#Process.login()
+	#Print.greeting()
 	while True:
 		try:
 			command = Command.read()
 			Command.execute(command)
 		except Exception as e:
-			print(Fore.RED + '>>> ' + Back.YELLOW + Fore.RED + 'Sorry, I could not perform that command.')
-			msg_problem = 'Problem: ' + str(e)
-			msg_len = len(msg_problem)
+			print(Fore.RED + '>>> ' + Back.LIGHTYELLOW_EX + Fore.RED + 'Sorry...!!!, I could not perform that command.')
+			msg_problem = 'Problem  : ' + str(e)
+			msg_info = 'INFO     : You can get more information about AMYCA CLI by entering \'help\' '
+			if len(msg_problem) > len(msg_info):
+				msg_len = len(msg_problem)
+			else:
+				msg_len = len(msg_info)
 			print(Fore.RED + '='*msg_len)
 			print(Fore.RED + msg_problem)
-			print(Fore.RED + '=' *msg_len)
+			print(Fore.MAGENTA + msg_info)
+			print(Fore.RED + '='*msg_len)
 
 
 if __name__ == '__main__':
