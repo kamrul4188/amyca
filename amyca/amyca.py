2cafdfc702b8cf5bcdfdbe4fc7a754faec7a9769
kamrul4188 <kamrul4188@gmail.com>
2019-04-18 00:25:16 +0800

    updated docstring

--- amyca/amyca.py
+++ amyca/amyca.py
@@ -38,6 +38,9 @@ Additional Functionality:
 	a. Minimum length of password is 6. Amyca not add user with below minimum length.
 	b. Stored user particular into hard disk with password as hash (encrypted)
 
+**********************************************
+NOTE: Default User: admin, Password: admin123
+**********************************************
 """
 import datetime
 import logging
