416eb4680296581abc46c2bfae02ff746cd8279e
kamrul4188 <kamrul4188@gmail.com>
2019-04-19 00:13:40 +0800

    add docstring

--- amyca/user.py
+++ amyca/user.py
@@ -1,8 +1,14 @@
+"""
+Module name: user
+This module is to create user object.
+"""
+
 import password as pw
 import csv
 
 
 class User:
+	"""This class is to create user object"""
 	__users = []
 	__total = 0
 	__current_user_name = ''
@@ -13,6 +19,12 @@ class User:
 	INDEX_USER_ACCESS_LEVEL = 2
 
 	def __init__(self, user_name, password, access_level):
+		"""
+		This function is to initialize user class.
+		:param user_name: username/ID
+		:param password: password to be set for the user
+		:param access_level: user access level
+		"""
 		self.__user_name = user_name
 		self.__password = pw.Password.hash(password)
 		self.__access_level = int(access_level)
@@ -21,6 +33,12 @@ class User:
 
 	@classmethod
 	def verify(cls, name, password):
+		"""
+		This function is to verify user with database.
+		:param name: user name
+		:param password: user password
+		:return: bool
+		"""
 		try:
 			for i, user in enumerate(cls.__users):
 				temp_user_name = cls.__users[i][cls.INDEX_USER_NAME]
@@ -35,6 +53,12 @@ class User:
 
 	@classmethod
 	def remove(cls, name, password):
+		"""
+		This function is to remove user from database.
+		:param name: user name
+		:param password: user password
+		:return: confirmation
+		"""
 		user_name = name
 		user_password = password
 		for i, user in enumerate(cls.__users):
@@ -46,6 +70,13 @@ class User:
 
 	@classmethod
 	def change_password(cls, name, current_password, new_password):
+		"""
+		This function is to change registered user password.
+		:param name: usr name
+		:param current_password: user current password
+		:param new_password: user new password
+		:return: confirmation.
+		"""
 		user_name = name
 		current_password = current_password
 		new_password = pw.Password.hash(new_password)
@@ -57,22 +88,43 @@ class User:
 
 	@classmethod
 	def get_total(cls):
+		"""
+		This function is to get total registered users.
+		:return: total user
+		"""
 		return cls.__total
 
 	@classmethod
 	def get_current_user_name(cls):
+		"""
+		This function is to get current user login / verify
+		:return: user name
+		"""
 		return cls.__current_user_name
 
 	@classmethod
 	def get_current_user_access_level(cls):
+		"""
+		This function is to get current user access level
+		:return: user level
+		"""
 		return cls.__current_user_level
 
 	@classmethod
 	def get_users(cls):
+		"""
+		This function is to get all user
+		:return: user list
+		"""
 		return User.__users
 
 	@classmethod
 	def save_as_csv(cls, storage):
+		"""
+		This function is to save user object into storage
+		:param storage: path of storage
+		:return: confirmation
+		"""
 		output_file = open(storage, 'w', newline='')
 		output_writer = csv.writer(output_file)
 		for user in User.__users:
@@ -82,6 +134,11 @@ class User:
 
 	@classmethod
 	def load_form_csv(cls, storage):
+		"""
+		This function is to get/load user data from storage
+		:param storage: path of user data
+		:return: confirmation
+		"""
 		file = open(storage)
 		deliveries_reader = csv.reader(file)
 		for row in deliveries_reader:
@@ -90,21 +147,14 @@ class User:
 
 		return 'User is loaded'
 
-
 	def __str__(self):
+		"""
+		This function run during initialization
+		:return: confirmation
+		"""
 		return 'New User added. [' + self.__user_name + ']'
 
 
 if __name__ == '__main__':
-	#User('admin', 'admin123', 4)
-	#User('kamrul', 'kamrul123', 3)
-	#print(User.get_users())
-	#User.remove('admin', 'admin123')
-	#User.change_password('admin', 'admin123', '123456')
-	#print('After: ', User.get_users())
-	#User.save_as_csv('program_data/users.csv')
-	print('befour load', User.get_users())
-	User.load_form_csv('program_data/users.csv')
-	print('After load', User.get_users())
-
+	pass
 
